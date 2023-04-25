3607044d2fc4ce0ad22ae2d6ee77cc57add2da28
Gerik Huland <gerik.huland@daimler.com>
2023-04-25 19:03:29 +0200

    some exercism

--- src/DNA.hs
+++ src/DNA.hs
@@ -1,16 +1,23 @@
-module DNA (toRNA) where
+module DNA (nucleotideCounts, Nucleotide(..)) where
+-- module DNA (toRNA) where
+import Data.Map (Map)
+import qualified Data.Map as Map
 
-toRNA :: String -> Either Char String
-toRNA xs
-    | not(null inval) = Left (head inval)
-    | otherwise = Right mapped
-    where mapped = map dnaMap xs
-          inval = filter (`notElem` "GCTA") xs
+data Nucleotide = A | C | G | T deriving (Eq, Ord, Show)
 
-dnaMap :: Char -> Char
-dnaMap x
-  | x == 'G' = 'C'
-  | x == 'C' = 'G'
-  | x == 'T' = 'A'
-  | x == 'A' = 'U'
-  | otherwise = 'x'
+nucleotideCounts :: String -> Either String (Map Nucleotide Int)
+nucleotideCounts xs = traverse countN
+    where
+        countN :: Char -> Either String (Map Nucleotide Int)
+        countN c = Right (Map.adjust (1 +) (pure mapN c) cmap)
+          where cmap = Map.fromList [(A, 0), (C, 0), (G, 0), (T, 0)]
+
+
+
+mapN :: Char -> Either String Nucleotide
+mapN c
+  | c == 'A' = pure A
+  | c == 'C' = pure C
+  | c == 'G' = pure G
+  | c == 'T' = pure T
+  | otherwise = Left "error"
