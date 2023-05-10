860eb9c4994b0fa589a37143b3add8d556dede88
Gerik Huland <gerik.huland@mercedes-benz.com>
2023-05-10 20:35:29 +0200

    wip

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,9 +1,27 @@
 module Lib
-  ( pal,
+  (bubbleSort
   )
 where
 
-pal :: String -> Bool
-pal xs
-  | xs == reverse xs = True
-  | otherwise = False
+bubbleSort :: (Ord a) => [a] -> [a]
+bubbleSort [] = []
+bubbleSort [x] = [x]
+bubbleSort xs = sorted
+  where (_, sorted) = bSort xs []
+
+
+bSort :: (Ord a) => [a] -> [a] -> ([a], [a])
+bSort [] [] = ([], [])
+bSort [] _ = ([], [])
+-- bSort [x] [] = ([], [x])
+bSort [x] ys = ([], x:ys)
+bSort [x,y] ys
+  | x <= y = ([], x:y:ys)
+  | otherwise = ([], y:x:ys)
+bSort (x:y:zs) ys =
+  if x <= y then
+    let (lower, upper) = bSort (y:zs) ys
+    in (x:lower, upper)
+  else
+    let (lower, upper) = bSort (x:zs) ys
+  in (y:lower, upper)
