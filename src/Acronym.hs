3bdfb3c95cb4bfa38d30e2e7d453cb63b2abd6e6
GH <hidden@hidden.private>
2023-05-15 09:54:22 +0200

    wip

--- src/Acronym.hs
+++ src/Acronym.hs
@@ -1,14 +1,21 @@
 module Acronym (abbreviate) where
 
-import Data.Text ()
-import qualified Data.Text as T
-import Data.Char (isAlphaNum, isLower, toUpper)
+import Data.Char (toUpper, isUpper, isAlphaNum)
+import Data.List
 
 abbreviate :: String -> String
-abbreviate = 
-      T.unpack 
-    . T.concat
-    . map (T.singleton . Data.Char.toUpper . T.head . T.filter isAlphaNum)  
-    . T.words 
-    . T.pack 
+abbreviate xs =
+    concat 
+    $ map (\(f:w) -> toUpper f:w)
+    $ map (filter isAlphaNum)
+    $ words xs
 
+
+   
+
+windowed :: Int -> [a] -> [[a]]
+windowed _ [] = [[]]
+windowed s lst@(_:xs) 
+  | length lst == s = [lst]
+  | otherwise = take s lst: windowed s xs 
+    
